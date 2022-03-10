22a925cc4e3939fc9fbf147d2d5b0c395d09ea15
MishC <michaela.chovancova@gmail.com>
2022-03-10 19:18:11 +0100

    not used component

---
+++ src/Explanation.js
@@ -0,0 +1,30 @@
+import React from "react";
+import 'Explanation.css';
+
+export default function Explanation(props) {
+
+const hiddenContent = (Desc) => {
+         setUrl(ability.ability.url)
+
+         Desc.className === "Desc_off" ? Desc.className = "Desc_on": Desc.className = "Desc_off"
+       };
+  if (props.abilities) { return(
+   {
+     props.abilities.map((ability, index) => {
+
+       // let url = ability.ability.url;
+
+       return (
+         <div>
+           <p key={index} onClick={hiddenContent(span) }>
+             {ability.ability.name}
+           </p>
+           <span  className="Desc_off" > </span>
+         </div>
+       );
+     })
+   })
+  } else {
+    return null;
+  }
+}
