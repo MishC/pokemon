009ba3b894b49a7c19bc3c344a9623e2fdb48ff5
MishC <michaela.chovancova@gmail.com>
2022-04-26 14:48:08 +0200

    Abilities rectangle not rounded

--- src/Abilities.js
+++ src/Abilities.js
@@ -35,7 +35,7 @@ export default function Abilities(props) {
 
   if (props.abilities) {
     return (
-      <div className="Abilities bg-white  pt-5 w-80 rounded m-0  border-top border-secondary">
+      <div className="Abilities bg-white  pt-5 w-80 m-0  border-top border-secondary">
         {/* <button class="btn info m-4 ">
             <h2>Moves</h2>
             
